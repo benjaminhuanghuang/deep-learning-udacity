61d56bb1867b6d5d243f1790af1eb790092ff975
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-11-01 12:04:00 -0700

    create projecty

---
+++ README.md
@@ -0,0 +1,6 @@
+## Deep Learning Course
+  https://classroom.udacity.com/courses/ud730
+
+
+## Setup
+  
---
+++ softmax.py
@@ -0,0 +1,17 @@
+'''
+  Define the function softmax to compute the softmax probability values
+'''
+
+import numpy as np
+scores = [3.0. 1.0, 0.2]
+def softmax(x):
+  pass
+
+print sofmax(scores)
+
+import matplotlib.pyplot as plt
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+plt.plot(x, softmax(scores).T, linewidth = 2)
+plt.show()
+
